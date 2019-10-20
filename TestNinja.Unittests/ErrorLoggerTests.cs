0c1bb7f43cb2596e44b7de07df710bbedff9d162
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 13:49:31 +0700

    TestingVoidMethods

---
+++ TestNinja.Unittests/ErrorLoggerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using _3_5_TestingVoidMethods.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.Unittests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
---
+++ TestNinja.Unittests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.Unittests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.Unittests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b7bc84c-1c48-41eb-b31c-4e62a0070dbc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
